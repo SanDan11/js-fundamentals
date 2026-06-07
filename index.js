9524e83e9b69c2b097bed923b007fe1db802903b
Daniel <Danielrsanchez1990@gmail.com>
2026-06-07 10:08:36 -0700

    updated: JavaScript updated

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
-function takeDamage(damage) {
-    console.log(`You take ${damage} damage!`);
+const bassInventory = [
+    {brand: "Ibanez", model: "GSRN2", tuning: "D standard"},
+    {brand: "Jackson", model: "JS3QV", tuning: "C standard"},
+    {brand: "Fender", model: "American Professional", tuning: "Standard"},
+    {brand: "Aria", model: "SB-1000", tuning: " Eb Standard"}
+]
+
+for (const bass of bassInventory) {
+    console.log(`${bass.brand} ${bass.model} - ${bass.tuning}`)
 }
